16d64601bb1313276ba8b64fc7c5781e61c1fd90
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-04-26 22:49:33 +0000

    mas restructuracion

--- php/clasesImportantes/bd.php
+++ php/clasesImportantes/bd.php
@@ -1,23 +1,28 @@
 
 <?php
-/*clase para la coneccion a la base de datos*/
-class bd{
+
+/* clase para la coneccion a la base de datos */
+
+class bd {
+
     private $db;
-	function __construct($user="ilove",$pass="running"){
-		$usuario = $user;
-		$password = $pass;
-		$host = "localhost";
-		$dbname = "scrab";
-		$this->db = new mysqli($host,$usuario,$password,$dbname);
-		if($this->db->connect_error){
-                    die("Error de conexion(".$this->db->connect_errno.")".$this->db->connect_error);
-		}
-                //else return $this->db;
-	}
-	
-	function getDB(){
-		return $this->db;
-	}
+
+    function __construct($user = "ilove", $pass = "running") {
+        $usuario = $user;
+        $password = $pass;
+        $host = "localhost";
+        $dbname = "scrab";
+        $this->db = new mysqli($host, $usuario, $password, $dbname);
+        if ($this->db->connect_error) {
+            die("Error de conexion(" . $this->db->connect_errno . ")" . $this->db->connect_error);
+        }
+        //else return $this->db;
+    }
+
+    function getDB() {
+        return $this->db;
+    }
+
 }
 
 ?>
--- php/modelo/UserDAO.php
+++ php/modelo/UserDAO.php
@@ -1,73 +1,105 @@
 <?php
+
 //debido a un problema con el sistema de ruta de window al estar en una maquina virtual
 //hemos de tener que hacer a mano las rutas de la aplicacion 
 //la otra opcion es cambiar el path de php.ini
 
-$ds="/";//variable con el separador
-$base_dir= realpath(dirname(__FILE__).$ds.'..').$ds;
+$ds = "/"; //variable con el separador
+$base_dir = realpath(dirname(__FILE__) . $ds . '..') . $ds;
 //definimos como directorio base el directorio en el que estamos en este caso es /php
 require_once("{$base_dir}clasesImportantes{$ds}bd.php");
-class UserDAO{
 
+class UserDAO {
     //definimos las tablas con las que intercambian datos los usuarios como constantes
-   const tablaPersonas="personas";
-   const tablaUsuarios="usuarios";
-   //definimos las propiedades
-   private $db;
-   
-    function __construct(){// Constructor de la clase
-		$obj = new bd();
-		$this->db = $obj->getDB();
-	}
 
-    function userpass(){// Comprueba si el usuario y la contraseña introducidos son correctos
-		$user = $_POST['user'];
-		$pass = hash("sha512",$_POST['pass']);
-		$result = $this->db->query("SELECT * FROM usuarios WHERE nombreUser = '$user' AND pass = '$pass'");
-		if($result->num_rows != 0){
-			$_SESSION['user'] = $_POST['user'];
-			header ("location: inicio.php");
-		}
-	}
-        
-       
-    function registroUsuario(){ // Hace el registro primero de persona y despues de usuario
-		$nombre = $_POST['nom'];
-		$ape1 = $_POST['ape1'];
-		$ape2 = $_POST['ape2'];
-		$nac = $_POST['nac'];
-		$sexo = $_POST['sexo'];
-		$nombreUser = $_POST['user'];
-		$email = $_POST['email'];
-		$pass = hash("sha512",$_POST['pass']);
-		$formato = null;
-		$foto = null;
-                $query1 = "SELECT MAX(idpersonas) AS \"mayor\" FROM personas";
-		$result1 = $this->db->query($query1);
-		$mayor = $result1->fetch_assoc();
-		$mayor1 = $mayor['mayor']+1;
-		$query2 = "INSERT INTO personas VALUES($mayor1,'user','$nombre','$ape1','$ape2','$nac','$sexo','$foto','$formato',0)";
-		$result = $this->db->query($query2);
-		$query3 = "INSERT INTO usuarios VALUES ($mayor1,'$nombreUser','$email','".time()."','',0,'$pass')";
-		$this->db->query($query3);
-		header ("location: index.php");
-		$_SESSION['user'] = $_POST['user'];
-	}
+    const tablaPersonas = "personas";
+    const tablaUsuarios = "usuarios";
 
-	function cerrarSesion(){
-		session_destroy();
-		header("location:index.php");
-	}
+    //definimos las propiedades
+    private $db;
 
-	function nombreUser(){// Comprueba si el nombre de usuario está libre
-		$user = $_POST['user'];
-		$result = $this->db->query("SELECT * FROM USUARIOS WHERE nombreUser = '$user'");
-		if($result->num_rows != 0){
-			return TRUE;
-		}else{
-			return FALSE;
-		}
-	}
+    public function __construct() {// Constructor de la clase
+        $obj = new bd();
+        $this->db = $obj->getDB();
+    }
+
+    function userpass() {// Comprueba si el usuario y la contraseña introducidos son correctos
+        $user = $_POST['user'];
+        $pass = hash("sha512", $_POST['pass']);
+        $result = $this->db->query("SELECT * FROM " . self::tablaUsuarios . " WHERE nombreUser = '$user' AND pass = '$pass'");
+        if ($result->num_rows != 0) {
+            $_SESSION['user'] = $_POST['user'];
+            header("location: inicio.php");
+        }
+    }
+
+    function registroUsuario() { // Hace el registro primero de persona y despues de usuario
+        $nombre = $_POST['nom'];
+        $ape1 = $_POST['ape1'];
+        $ape2 = $_POST['ape2'];
+        $nac = $_POST['nac'];
+        $sexo = $_POST['sexo'];
+        $nombreUser = $_POST['user'];
+        $email = $_POST['email'];
+        $pass = hash("sha512", $_POST['pass']);
+        $formato = null;
+        $foto = null;
+        //para hacer el registro primero tenemos que comprobar que no existe un campo
+        //con el mismo nombre de usuario ni el mismo correo en la bd
+        if ($this->emailUser($email) && $this->nombreUser($nombreUser)) {
+            //no eciste el email ni el nombre de usuario
+            $query1 = "SELECT MAX(idpersonas) AS \"mayor\" FROM personas";
+            $result1 = $this->db->query($query1);
+            $mayor = $result1->fetch_assoc();
+            $mayor1 = $mayor['mayor'] + 1;
+            //insertamos datos en personas
+            $query2 = "INSERT INTO " . self::tablaPersonas . " VALUES($mayor1,'user','$nombre','$ape1','$ape2','$nac','$sexo','$foto','$formato',0)";
+            if (!$this->db->query($query2)) {
+                //echo $this->db->errno;
+                throw die("eror introduciendo los datos en la tabla personas");
+            }
+
+            //migramos los datos de personas a aqui
+            $query3 = "INSERT INTO " . self::tablaUsuarios . " VALUES ($mayor1,'$nombreUser','$email','" . time() . "','',0,'$pass')";
+            if (!$this->db->query($query3)) {
+                //echo $this->db->errno;
+                throw die("error introduciendo los datos en la tabla usuarios");
+            }
+        } else {
+            throw die("el eemail o el nombre de usuario introducido no son validos");
+        }
+
+        //obtenemos el ultimos usuario
+
+
+        header("location: index.php");
+        $_SESSION['user'] = $_POST['user'];
+    }
+
+    function cerrarSesion() {
+        session_destroy();
+        header("location:index.php");
+    }
+
+    private function nombreUser($user) {// Comprueba si el nombre de usuario está libre
+        //$user = $_POST['user'];
+        $result = $this->db->query("SELECT nombreUser FROM " . self::tablaUsuarios . " WHERE nombreUser = '$user'");
+        if ($result->num_rows == 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    private function emailUser($email) {
+        $result = $this->db->query("SELECT nombreUser FROM " . self::tablaUsuarios . " WHERE email = '$email'");
+        if ($result->num_rows == 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
 }
 
 ?>
