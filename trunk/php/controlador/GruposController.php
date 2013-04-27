c021b49b87a065df866297f91fcbfd2b8a94a555
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-04-27 11:14:01 +0000

    mas restructuracion

---
+++ trunk/php/controlador/GruposController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GruposController
+ *
+ * @author nico
+ */
+class GruposController {
+    //put your code here
+}
+
+?>
