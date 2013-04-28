358533a6ee49190c10d1f823e4a40b3922555f30
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-04-28 22:35:17 +0000

    mas restructuracion

--- trunk/php/controlador/GruposController.php
+++ trunk/php/controlador/GruposController.php
@@ -1,17 +1,30 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * autor : nico
+ * descripcion: codigo del controlador al recibir una peticion ajax
  */
 
-/**
- * Description of GruposController
- *
- * @author nico
- */
-class GruposController {
-    //put your code here
+$base_dir = realpath(dirname(__FILE__) .'/..');
+//definimos como directorio base el directorio en el que estamos en este caso es /php
+require_once("{$base_dir}/modelo/gruposDAO.php");
+
+
+$dir='image/';
+$name = $_POST['name'];
+$var2= $_POST['description'];
+
+$obj = new GruposDAO();
+if($obj->insert(2, 1, $name,  $var2)){
+    echo "<h1>exito</h1>";
 }
+ else {
+    "<h1>fracaso</h1>";
+}
+
+
+//class GruposController {
+    //put your code here
+//}
 
 ?>
