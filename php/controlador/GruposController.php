5f85eef8f835b37acf0189a1fc20c6520193675e
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-05-01 16:35:50 +0000

    isma gay

--- php/controlador/GruposController.php
+++ php/controlador/GruposController.php
@@ -5,25 +5,47 @@
  * descripcion: codigo del controlador al recibir una peticion ajax
  */
 
-$base_dir = realpath(dirname(__FILE__) .'/..');
+$base_dir = realpath(dirname(__FILE__) . '/..');
+//echo $base_dir;
 //definimos como directorio base el directorio en el que estamos en este caso es /php
 require_once("{$base_dir}/modelo/gruposDAO.php");
 
 
-if(!isset($_POST['image'])){
-    echo "ha llegado la imagen";
+
+//$dirImg=  realpath(dirname(__FILE__).'../../image/grupo');
+//echo $dirImg;
+
+
+
+if (isset($_POST['image']) && isset($_POST['imageName'])) {
+    $file = $_POST['image'];
+    $name = $_POST['imageName'];
+    $data = explode(',', $file);
+    // Encode it correctly
+    $encodedData = str_replace(' ', '+', $data[1]);
+    $decodedData = base64_decode($encodedData);
+    if (file_put_contents( $base_dir.$name, $decodedData)) {
+        //echo $dirImg;
+        echo $name . ":uploaded successfully";
+    } else {
+        // Show an error message should something go wrong.
+        echo "Something went wrong. Check that the file isn't corrupted";
+    }
+
+
+    //echo "ha llegado la imagen";
 }
-$dir='image/';
+
+
 $name = $_POST['name'];
-$description= $_POST['description'];
+$description = $_POST['description'];
 
 $obj = new GruposDAO();
-$id=$obj->ultimoDato();
+$id = $obj->ultimoDato();
 
-if($obj->insert_datos($id, 1, $name, $description)){
+if ($obj->insert_datos($id, 1, $name, $description)) {
     echo "<h1>exito</h1>";
-}
- else {
+} else {
     "<h1>fracaso</h1>";
 }
 
@@ -31,7 +53,6 @@ echo $id;
 echo "<br>$name<br>$description";
 
 //class GruposController {
-    //put your code here
+//put your code here
 //}
-
 ?>
