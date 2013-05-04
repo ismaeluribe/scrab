aa7c6a2c9016d90f52f20838f838130ee17cb3da
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-05-04 19:36:54 +0000

    Restructuracion de la clase usuario a usuario y personas, script para la vaalidacion del formulario de registro, controlador para la clase de registro, creacion de la clase dao persona, manejo de excepciones en el modelo

---
+++ trunk/php/modelo/modeloException/ModeloException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ModeloException
+ *
+ * @author nico
+ */
+class ModeloException {
+    //put your code here
+}
+
+?>
