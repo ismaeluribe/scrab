4ed60ee71d66427e69d1b5ba87652e44dd7fad47
proyectoDawIsNi@gmail.com <proyectoDawIsNi@gmail.com@5c8f81a3-e98b-f8ab-eaed-0e681959e0bb>
2013-05-04 20:11:43 +0000

    problema para actualizarme

---
+++ trunk/php/modelo/modeloException/ModeloException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ModeloException
+ *
+ * @author nico
+ */
+class ModeloException extends RuntimeException{
+    //put your code here
+}
+
+?>
